544557db307d6ba25026066a051ec2a7150a3d5f
dfsalam <dfsalam@gmail.com>
2024-02-26 20:42:53 -0500

    Create admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "home#index"
+
+  #to change home page when admin is logged
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
+
 end
